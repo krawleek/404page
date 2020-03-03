96dbb051415cc0962e3e40735ef8c964daf0cf33
Elena <lenusya.solodovnikova@gmail.com>
2020-03-03 14:39:41 +0300

    строка - бесконечная

--- js/js.js
+++ js/js.js
@@ -25,3 +25,28 @@ $(function() {
   });
 
 });
+
+
+		$(function() {
+
+			var marquee = $("#marquee"); 
+			marquee.css({"overflow": "hidden", "width": "100%"});
+
+			// �������� ��� ������ ����� span (IE �� ����� ���� � inline-block)
+			marquee.wrapInner("<span>");
+			marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" }); 
+			marquee.append(marquee.find("span").clone()); // ��� � ��� ��� span � �������
+
+			marquee.wrapInner("<div>");
+			marquee.find("div").css("width", "200%");
+
+			var reset = function() {
+				$(this).css("margin-left", "0%");
+				$(this).animate({ "margin-left": "-100%" }, 12000, 'linear', reset);
+			};
+
+			reset.call(marquee.find("div"));
+
+		});
+
+  
